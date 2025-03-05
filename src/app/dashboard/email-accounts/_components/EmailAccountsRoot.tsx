2c1b087b2d936599a3001acd4450c525717d5767
Md. Taufiqur Rahman <56925314+MdTaufiqurRahman@users.noreply.github.com>
2025-03-05 09:45:00 +0600

    added add new email accounts page

--- src/app/dashboard/email-accounts/_components/EmailAccountsRoot.tsx
+++ src/app/dashboard/email-accounts/_components/EmailAccountsRoot.tsx
@@ -1,12 +1,20 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 import { CgExport } from "react-icons/cg";
 import { FiPlusCircle } from "react-icons/fi";
 
 const EmailAccountsRoot = () => {
+  const router = useRouter();
+
+  const handleAddNew = () => {
+    router.push("/dashboard/email-accounts/new");
+  };
   return (
     <div>
       <section className="flex justify-between items-center">
-        <Button variant="primary" size="lg">
+        <Button variant="primary" size="lg" onClick={handleAddNew}>
           <FiPlusCircle className="text-xl" />
           Add new
         </Button>
---
+++ src/app/dashboard/email-accounts/new/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { IoIosArrowBack } from "react-icons/io";
+
+export default function AddNewEmailAccounts() {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.push("/dashboard/email-accounts");
+  };
+
+  return (
+    <div className="p-6">
+      <Button variant="outline" size="lg" onClick={handleBack}>
+        <IoIosArrowBack className="text-xl" />
+        Back
+      </Button>
+      <h1 className="font-semibold text-xl mt-6"> Add Email Account</h1>
+
+      <p className="font-normal text-base mt-2">
+        Add your email account to begin your campaign
+      </p>
+    </div>
+  );
+}
