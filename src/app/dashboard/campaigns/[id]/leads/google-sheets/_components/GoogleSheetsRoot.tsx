6f169cdf5fd95c70e9d7f46a6baee5b6ad9bfd87
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2025-04-10 01:07:27 +0600

    :sparkles: feat: google sheets page

--- src/app/dashboard/campaigns/[id]/leads/google-sheets/_components/GoogleSheetsRoot.tsx
+++ src/app/dashboard/campaigns/[id]/leads/google-sheets/_components/GoogleSheetsRoot.tsx
@@ -1,7 +1,18 @@
+import { CircleAlert } from 'lucide-react'
 import React from 'react'
+import GoogleSheetsForm from './GoogleSheetsForm'
 
 export default function GoogleSheetsRoot() {
     return (
-        <div>GoogleSheetsRoot</div>
+        <div className='my-6'>
+            <h3 className='text-lg lg:text-2xl font-semibold text-slate-800'>Use Google Sheets</h3>
+
+            <div className='py-3 px-5 bg-gray-100 rounded-md flex items-center gap-2 mt-4'>
+                <CircleAlert className='w-5 h-5 text-slate-800' />
+                <span className='text-slate-800 font-medium'>Make sure your Google Sheet is publicly accessible</span>
+            </div>
+
+            <GoogleSheetsForm />
+        </div>
     )
 }
